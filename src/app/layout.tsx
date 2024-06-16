6c33deb2d3864977928eb1cf0cdf0d90ee2cb9ab
johnymre <johnermy@gmail.com>
2024-06-16 17:46:59 +0800

    Wedding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
       <head>
       <meta property="og:image" content="https://johnermy.vercel.app/featuredimg.png" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       <link href="https://fonts.googleapis.com/css2?family=Pinyon+Script&display=swap" rel="stylesheet" />
       </head>
       <body className="body">
